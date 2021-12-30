7349b3b79156d8f976755b0b8a9ec9aea3999989
Masayuki Ono <mono0926@gmail.com>
2021-12-30 11:47:06 +0900

    Implement SettingsService

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final settingsController = SettingsController(SettingsService());
   await settingsController.loadSettings();
   runApp(MyApp(settingsController: settingsController));
--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  static const _themeKey = 'themeKey';
 
-  Future<void> updateThemeMode(ThemeMode theme) async {}
+  Future<ThemeMode> themeMode() async {
+    final sp = await SharedPreferences.getInstance();
+    final themeString = sp.getString(_themeKey);
+    return themeString == null
+        ? ThemeMode.system
+        : ThemeMode.values.byName(themeString);
+  }
+
+  Future<void> updateThemeMode(ThemeMode theme) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_themeKey, theme.name);
+  }
 }
